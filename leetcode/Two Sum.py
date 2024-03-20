42949c87594d5f55e80009a21672c5dcc9943ebd
gajulaakanksha <gajulaakanksha02@gmail.com>
2024-03-20 20:32:20 +0530

    two sum

---
+++ leetcode/Two Sum.py
@@ -0,0 +1,9 @@
+nums=[2,7,11,15]
+target=9
+for i in range(len(nums)):
+    for j in range(i+1,len(nums)):
+        if target==nums[i]+nums[j]:
+            print([i,j])
+            break
+                
+        
