a23d8c1675f4f84703ce165b69d5baf0090a8cf6
gajulaakanksha <gajulaakanksha02@gmail.com>
2024-03-21 17:06:08 +0530

    ransomNote and magazine

--- leetcode/ransom-note.py
+++ leetcode/ransom-note.py
@@ -1,11 +1,19 @@
+class Solution:
+    def __init__(self,ransomNote,magazine):
+         self.ransomNote=ransomNote
+         self.magazine=magazine
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+            a=set(ransomNote)
+            for i in a:
+                x=ransomNote.count(i)
+                y=magazine.count(i)
+                if y<x:
+                    return False
+                    break
+            else:
+                return True
 ransomNote="a"
-magazine="b"
-a=set(ransomNote)
-for i in a:
-    x=ransomNote.count(i)
-    y=magazine.count(i)
-    if y<x:
-        print(False)
-        break
-    else:
-        print(True)
+magazine="b"            
+solution=Solution(ransomNote,magazine)
+result=solution.canConstruct(ransomNote,magazine)
+print(result)
