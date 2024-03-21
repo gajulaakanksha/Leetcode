b4c6c81f7c8424b42c375f6e145f3cab230fdb1d
gajulaakanksha <gajulaakanksha02@gmail.com>
2024-03-21 17:21:51 +0530

    Two Sum

--- leetcode/Two Sum.py
+++ leetcode/Two Sum.py
@@ -1,9 +1,22 @@
+class Solution:
+    def __init__(self,nums,target):
+        self.nums=nums
+        self.target=target
+    def twoSum(self, nums: [int], target: int) -> [int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if target==nums[i]+nums[j]:
+                    return [i,j]
+                    break
 nums=[2,7,11,15]
 target=9
-for i in range(len(nums)):
-    for j in range(i+1,len(nums)):
-        if target==nums[i]+nums[j]:
-            print([i,j])
-            break
+solution=Solution(nums,target)
+result=solution.twoSum(nums,target)
+print(result)
+                
+        
+                
+        
+
                 
         
