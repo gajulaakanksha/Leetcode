9a3351ba40593cd94ce7b346d132ca330c729f9d
gajulaakanksha <gajulaakanksha02@gmail.com>
2024-04-02 21:57:48 +0530

    Valid Parentheses

---
+++ leetcode/validparentheses.py
@@ -0,0 +1,33 @@
+class Solution:
+    def __init__(self,s):
+        self.s=s
+    def isValid(self, s: str) -> bool:
+        while len(s) > 0:
+            l = len(s)
+            s = s.replace('()','').replace('{}','').replace('[]','')
+            if l==len(s): return False
+        return True
+s="()"
+solution=Solution(s)
+result=solution.isValid(s)
+print(result)
+
+
+                  
+
+    
+
+        
+                
+
+        
+
+
+                 
+
+    
+
+        
+                
+
+        
